da55a5f08551611da0a7925254c6c5e93f84ba6c
hunghq <hunghq@yopaz.vn>
2023-11-11 16:23:53 +0700

    project 6 Q4

--- Project 6/models.py
+++ Project 6/models.py
@@ -146,13 +146,11 @@ class DigitClassificationModel(object):
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
-        n_step = 1000
         learning_rate = -0.5
         while dataset.get_validation_accuracy() < 0.98:
             for x, y in dataset.iterate_once(100):
                 loss = self.get_loss(x, y)
-                grad_wrt_m1, grad_wrt_b1, grad_wrt_m2, grad_wrt_b2 = nn.gradients(loss,
-                                                                                  [self.m1, self.b1, self.m2, self.b2])
+                grad_wrt_m1, grad_wrt_b1, grad_wrt_m2, grad_wrt_b2 = nn.gradients(loss,[self.m1, self.b1, self.m2, self.b2])
                 self.m1.update(grad_wrt_m1, learning_rate)
                 self.b1.update(grad_wrt_b1, learning_rate)
                 self.m2.update(grad_wrt_m2, learning_rate)
@@ -176,6 +174,13 @@ class LanguageIDModel(object):
 
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        self.hSize = 600
+        self.wX = nn.Parameter(self.num_chars, self.hSize)
+        self.wHidden = nn.Parameter(self.hSize,self.hSize)
+        self.m1 = nn.Parameter(self.hSize, 500)
+        self.b1 = nn.Parameter(1, 500)
+        self.m2 = nn.Parameter(500, 5)
+        self.b2 = nn.Parameter(1, 5)
 
     def run(self, xs):
         """
@@ -207,6 +212,20 @@ class LanguageIDModel(object):
                 (also called logits)
         """
         "*** YOUR CODE HERE ***"
+        h = nn.Linear(xs[0], self.wX)
+        for i in range(1, len(xs)):
+            h_prev = h
+            h = nn.Add(nn.Linear(xs[i], self.wX), nn.Linear(h_prev, self.wHidden))
+
+        x = nn.Linear(h, self.m1)
+        # print(x)
+        x = nn.AddBias(x, self.b1)
+        x = nn.ReLU(x)
+        # print(x)
+        x = nn.Linear(x, self.m2)
+        x = nn.AddBias(x, self.b2)
+        # print(x)
+        return x
 
     def get_loss(self, xs, y):
         """
@@ -223,9 +242,20 @@ class LanguageIDModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
+        return nn.SoftmaxLoss(self.run(xs),y)
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        learning_rate = -0.05
+        while dataset.get_validation_accuracy() < 0.85:
+            for x, y in dataset.iterate_once(100):
+                loss = self.get_loss(x, y)
+                grad_wrt_m1, grad_wrt_b1, grad_wrt_m2, grad_wrt_b2 = nn.gradients(loss,
+                                                                                  [self.m1, self.b1, self.m2, self.b2])
+                self.m1.update(grad_wrt_m1, learning_rate)
+                self.b1.update(grad_wrt_b1, learning_rate)
+                self.m2.update(grad_wrt_m2, learning_rate)
+                self.b2.update(grad_wrt_b2, learning_rate)
